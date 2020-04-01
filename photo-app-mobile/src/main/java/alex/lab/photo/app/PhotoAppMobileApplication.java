2062ff419a98ba228e44dca9fc8ee6864da8bcb9
Alex <githubalexrom@gmail.com>
2020-04-01 14:33:12 -0400

    Initial commit

---
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/PhotoAppMobileApplication.java
@@ -0,0 +1,13 @@
+package alex.lab.photo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppMobileApplication.class, args);
+	}
+
+}
---
+++ photo-app-mobile/src/test/java/alex/lab/photo/app/PhotoAppMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.lab.photo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
