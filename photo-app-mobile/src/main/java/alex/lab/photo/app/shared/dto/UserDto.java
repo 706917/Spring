e5ff9472bc60457024c38830d5661c0af465d373
Alex <githubalexrom@gmail.com>
2020-04-01 20:43:42 -0400

    Create UserDto.java

---
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/shared/dto/UserDto.java
@@ -0,0 +1,94 @@
+package alex.lab.photo.app.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+
+	private static final long serialVersionUID = -2603836252299267619L;
+
+	private long id; // DataBase id
+	private String userId; // Public id
+
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	private String password; // Got from the user
+	private String encryptedPassword; // to Store in Database
+
+	private String emailVerificationToken;
+	private Boolean emeailVerificationStatus;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+
+	public String getEmailVerificationToken() {
+		return emailVerificationToken;
+	}
+
+	public void setEmailVerificationToken(String emailVerificationToken) {
+		this.emailVerificationToken = emailVerificationToken;
+	}
+
+	public Boolean getEmeailVerificationStatus() {
+		return emeailVerificationStatus;
+	}
+
+	public void setEmeailVerificationStatus(Boolean emeailVerificationStatus) {
+		this.emeailVerificationStatus = emeailVerificationStatus;
+	}
+
+}
