f324a21bb7f197792e7339a1ad6ae9c59781decc
Alex <githubalexrom@gmail.com>
2020-04-02 16:38:27 -0400

    Added - extends UserDetailsService - interface

--- photo-app-mobile/src/main/java/alex/lab/photo/app/ui/service/UserServise.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/ui/service/UserServise.java
@@ -1,8 +1,10 @@
 package alex.lab.photo.app.ui.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import alex.lab.photo.app.shared.dto.UserDto;
 
-public interface UserServise {
+public interface UserServise  extends UserDetailsService{
 	
 	UserDto createUser(UserDto user);
 
