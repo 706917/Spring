209f44b76ea4718fc05ebab2389f9eb8cd2dffd2
Alex <githubalexrom@gmail.com>
2020-04-01 20:43:53 -0400

    Create UserServiceIml.java

---
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/ui/service/imp/UserServiceIml.java
@@ -0,0 +1,17 @@
+package alex.lab.photo.app.ui.service.imp;
+
+import org.springframework.stereotype.Service;
+
+import alex.lab.photo.app.shared.dto.UserDto;
+import alex.lab.photo.app.ui.service.UserServise;
+
+@Service
+public class UserServiceIml implements UserServise{
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		
+		return null;
+	}
+
+}
