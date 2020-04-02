e43bb3cad0fa2ceeead33d8767a85665c22dd4ba
Alex <githubalexrom@gmail.com>
2020-04-02 16:41:08 -0400

    Create WebSecurity.java

---
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/security/WebSecurity.java
@@ -0,0 +1,38 @@
+package alex.lab.photo.app.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import alex.lab.photo.app.ui.service.UserServise;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	private final UserServise userDetailsService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public WebSecurity(UserServise userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		super();
+		this.userDetailsService = userDetailsService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests() // Allows restricting access based upon the HttpServletRequest using 
+		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll() // Allows any POST requests to "/users" path for all users
+		.anyRequest().authenticated(); // Any other requests should be authenticated
+	}
+
+}
