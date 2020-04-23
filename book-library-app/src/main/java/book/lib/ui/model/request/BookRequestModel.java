9ddcfad053b53afd3eab696671679b511e702c2e
Alex <githubalexrom@gmail.com>
2020-04-23 09:08:56 -0400

    Create BookRequestModel.java

---
+++ book-library-app/src/main/java/book/lib/ui/model/request/BookRequestModel.java
@@ -0,0 +1,30 @@
+package book.lib.ui.model.request;
+
+public class BookRequestModel {
+	
+	private String bookName;
+	private String authorName;
+	private String isbn;
+	
+	
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+}
