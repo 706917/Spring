abd483b3295fbdc4384c2457d7d35bc78894b341
Alex <githubalexrom@gmail.com>
2020-04-25 18:52:42 -0400

    Token_Secret moved to the property file

--- photo-app-mobile/src/main/java/alex/lab/photo/app/security/SecurityConstants.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/security/SecurityConstants.java
@@ -1,11 +1,20 @@
  package alex.lab.photo.app.security;
 
+import alex.lab.photo.app.SpringApplicationContext;
+
 public class SecurityConstants {
 	
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "d6bg5sf6b48sbbf";
+	
+	//public static final String TOKEN_SECRET = "d6bg5sf6b48sbbf";
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		return appProperties.getTokenSecret();
+	}
+	
+	
 
 }
