390ecdef72a5e7193b78e4fdfde59b3f25a9bc75
Alex <githubalexrom@gmail.com>
2020-04-25 18:51:22 -0400

    Bean declaration of appProperties added and commented out

--- photo-app-mobile/src/main/java/alex/lab/photo/app/PhotoAppMobileApplication.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/PhotoAppMobileApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import alex.lab.photo.app.security.AppProperties;
+
 @SpringBootApplication
 public class PhotoAppMobileApplication {
 
@@ -22,5 +24,9 @@ public class PhotoAppMobileApplication {
 		return new SpringApplicationContext();
 	}
 	
+//	@Bean(name="appProperties")
+//	public AppProperties getAppProperties() {
+//		return new AppProperties();
+//	}
 
 }
