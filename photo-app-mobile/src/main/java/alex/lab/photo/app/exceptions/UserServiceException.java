9d40141b90758664dc150c11b5e9d96ab0be3ce3
Alex <githubalexrom@gmail.com>
2020-04-27 10:28:54 -0400

    Create UserServiceException.java

---
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/exceptions/UserServiceException.java
@@ -0,0 +1,21 @@
+package alex.lab.photo.app.exceptions;
+
+public class UserServiceException extends RuntimeException{
+
+	private static final long serialVersionUID = -1915686512729667540L;
+
+	/**
+	 * @param message
+	 */
+	public UserServiceException(String message) {
+		super(message);
+	}
+	
+	
+		
+		
+		
+	
+	
+
+}
