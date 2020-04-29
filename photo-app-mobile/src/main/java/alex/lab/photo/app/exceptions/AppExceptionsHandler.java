4ce7cbbde3c18bb0360a45f6341faf22f2cfb606
Alex <githubalexrom@gmail.com>
2020-04-29 10:17:36 -0400

    misc

--- photo-app-mobile/src/main/java/alex/lab/photo/app/exceptions/AppExceptionsHandler.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package alex.lab.photo.app.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +9,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import alex.lab.photo.app.ui.model.responce.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 	
 	@ExceptionHandler(value = {UserServiceException.class})
-	public ResponseEntity<Object> handlerEserServiceException(UserServiceException ex, WebRequest request){
+	public ResponseEntity<Object> handlerUserServiceException(UserServiceException ex, WebRequest request){
 		
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
 
