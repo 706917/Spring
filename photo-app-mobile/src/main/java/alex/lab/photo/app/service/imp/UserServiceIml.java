52586ab8957e2f30254d8eb539aea5cb6b538154
Alex <githubalexrom@gmail.com>
2020-04-29 10:18:34 -0400

    A Method UpdateUser added

--- photo-app-mobile/src/main/java/alex/lab/photo/app/service/imp/UserServiceIml.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/service/imp/UserServiceIml.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.BeanDefinitionDsl.BeanSupplierContext;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import alex.lab.photo.app.exceptions.UserServiceException;
 import alex.lab.photo.app.io.entity.UserEntity;
 import alex.lab.photo.app.io.repositories.UserRepository;
 import alex.lab.photo.app.service.UserService;
 import alex.lab.photo.app.shared.Utils;
 import alex.lab.photo.app.shared.dto.UserDto;
+import alex.lab.photo.app.ui.model.responce.ErrorMessages;
 
 @Service
 public class UserServiceIml implements UserService {
@@ -54,6 +57,24 @@ public class UserServiceIml implements UserService {
 
 		return returnValue;
 	}
+	
+	@Override
+	public UserDto updateUser(String id, UserDto userDto) {
+		
+		UserDto returnValue = new UserDto();		
+		UserEntity userEntity = userRepository.findByUserId(id);
+		
+		if(userEntity==null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+		
+		userEntity.setFirstName(userDto.getFirstName());
+		userEntity.setLastName(userDto.getLastName() );
+		
+		UserEntity updatedUser = userRepository.save(userEntity);
+		BeanUtils.copyProperties(updatedUser, returnValue);
+		
+		return returnValue;
+	}
+	
 
 	@Override
 	public UserDto getUser(String email) {
@@ -87,5 +108,7 @@ public class UserServiceIml implements UserService {
 
 		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
+
+	
 
 }
