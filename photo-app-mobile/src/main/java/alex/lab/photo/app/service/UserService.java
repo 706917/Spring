232b9cb28db5d547a197b003447fd9c5aa16da7e
Alex <githubalexrom@gmail.com>
2020-04-29 10:18:54 -0400

    A method updateUser added

--- photo-app-mobile/src/main/java/alex/lab/photo/app/service/UserService.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/service/UserService.java
@@ -4,12 +4,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import alex.lab.photo.app.shared.dto.UserDto;
 
-public interface UserService  extends UserDetailsService{
-	
+public interface UserService extends UserDetailsService {
+
 	UserDto createUser(UserDto user);
-	
+
 	UserDto getUser(String email);
 
 	UserDto getUserByUserId(String id);
+
+	UserDto updateUser(String id, UserDto userDto);
 
 }
