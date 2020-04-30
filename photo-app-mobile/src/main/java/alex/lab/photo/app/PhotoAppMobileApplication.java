939afebfbaa46bb95f019eebdb300d3986bb59b3
Alex <githubalexrom@gmail.com>
2020-04-30 16:20:12 -0400

    extends SpringBootServletInitializer added

--- photo-app-mobile/src/main/java/alex/lab/photo/app/PhotoAppMobileApplication.java
+++ photo-app-mobile/src/main/java/alex/lab/photo/app/PhotoAppMobileApplication.java
@@ -2,13 +2,21 @@ package alex.lab.photo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import alex.lab.photo.app.security.AppProperties;
 
 @SpringBootApplication
-public class PhotoAppMobileApplication {
+public class PhotoAppMobileApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {		
+		return app.sources(PhotoAppMobileApplication.class);
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppMobileApplication.class, args);
@@ -23,6 +31,8 @@ public class PhotoAppMobileApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
+
+	
 	
 //	@Bean(name="appProperties")
 //	public AppProperties getAppProperties() {
