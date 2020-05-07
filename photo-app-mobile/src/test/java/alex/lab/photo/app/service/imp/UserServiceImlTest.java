cc486cb096f33b0418f4868bfa65d14300c655fd
Alex <githubalexrom@gmail.com>
2020-05-07 08:34:26 -0400

    misc

--- photo-app-mobile/src/test/java/alex/lab/photo/app/service/imp/UserServiceImlTest.java
+++ photo-app-mobile/src/test/java/alex/lab/photo/app/service/imp/UserServiceImlTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
