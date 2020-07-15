27692994e6f1def8700dec1901f3fda1117a4c87
Alex <githubalexrom@gmail.com>
2020-07-15 13:25:38 -0400

    userName property changed to name

--- todo-multi-user/src/main/java/lab/alex/todo/model/responce/UserResponceModel.java
+++ todo-multi-user/src/main/java/lab/alex/todo/model/responce/UserResponceModel.java
@@ -10,7 +10,7 @@ public class UserResponceModel {
 
 	private String publicUserId;
 
-	private String userName;
+	private String name;
 
 	private String phone;
 
@@ -32,12 +32,12 @@ public class UserResponceModel {
 		this.publicUserId = publicUserId;
 	}
 
-	public String getUserName() {
-		return userName;
+	public String getName() {
+		return name;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setName(String userName) {
+		this.name = userName;
 	}
 
 	public String getPhone() {
