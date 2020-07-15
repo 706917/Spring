8a08a5543d60051b35573c309cc75d18a173276a
Alex <githubalexrom@gmail.com>
2020-07-15 09:37:04 -0400

    Create TodoResponceModel.java

---
+++ todo-multi-user/src/main/java/lab/alex/todo/model/responce/TodoResponceModel.java
@@ -0,0 +1,52 @@
+package lab.alex.todo.model.responce;
+
+import java.util.Date;
+
+public class TodoResponceModel {
+	
+	//********************Attributes***************
+
+		private String publicTodoId;
+
+		private String textTodo;
+
+		private Date dateTimeTodoCreated;
+
+		private boolean completed;
+		
+		
+		//*************Accessors*******************
+
+		public String getPublicTodoId() {
+			return publicTodoId;
+		}
+
+		public void setPublicTodoId(String publicTodoId) {
+			this.publicTodoId = publicTodoId;
+		}
+
+		public String getTextTodo() {
+			return textTodo;
+		}
+
+		public void setTextTodo(String textTodo) {
+			this.textTodo = textTodo;
+		}
+
+		public Date getDateTimeTodoCreated() {
+			return dateTimeTodoCreated;
+		}
+
+		public void setDateTimeTodoCreated(Date dateTimeTodoCreated) {
+			this.dateTimeTodoCreated = dateTimeTodoCreated;
+		}
+
+		public boolean isCompleted() {
+			return completed;
+		}
+
+		public void setCompleted(boolean completed) {
+			this.completed = completed;
+		}
+
+}
