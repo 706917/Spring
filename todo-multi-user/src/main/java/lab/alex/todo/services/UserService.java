b739d517be4caa8fd9b9a11f7fbe71f396ecb44e
Alex <githubalexrom@gmail.com>
2020-07-15 13:27:32 -0400

    three new methods added

--- todo-multi-user/src/main/java/lab/alex/todo/services/UserService.java
+++ todo-multi-user/src/main/java/lab/alex/todo/services/UserService.java
@@ -1,9 +1,18 @@
 package lab.alex.todo.services;
 
+import java.util.List;
+
 import lab.alex.todo.dto.UserDto;
 
 public interface UserService {// extends UserDetailsService{
 	
 	UserDto createUser(UserDto userDto) throws Exception;
+	
+	List <UserDto> getAllUsers();
+	
+	UserDto getUserByUserId(String id) throws Exception;
+	
+
+	UserDto updateUser(String id, UserDto userDto) throws Exception;
 
 }
