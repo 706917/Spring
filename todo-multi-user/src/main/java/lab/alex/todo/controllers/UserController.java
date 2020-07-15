0e6ee007b7c7db08d2235cc566e49a8425b6ef8d
Alex <githubalexrom@gmail.com>
2020-07-15 09:36:37 -0400

    @Postmapping first version complited. ModelMapper autowired

--- todo-multi-user/src/main/java/lab/alex/todo/controllers/UserController.java
+++ todo-multi-user/src/main/java/lab/alex/todo/controllers/UserController.java
@@ -3,17 +3,34 @@ package lab.alex.todo.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lab.alex.todo.dto.UserDto;
+import lab.alex.todo.model.request.UserDetailsRequestModel;
+import lab.alex.todo.model.responce.UserResponceModel;
+import lab.alex.todo.services.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+	
+	// *************** DJs *****************
+	@Autowired
+	ModelMapper mapper;
+	@Autowired
+	UserService userService;
+	
+	
+	// **************** Controllers **************
 	
 	@GetMapping
 	public String getAllUsers(){
@@ -29,9 +46,16 @@ public class UserController {
 		
 	}
 	
-	@PostMapping("/{id}")
-	public String createUser(@PathVariable String id) {
-		return "Created";
+	@PostMapping
+	public UserResponceModel createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel) throws Exception {
+		
+		UserDto userDto = mapper.map(userDetailsRequestModel, UserDto.class);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		
+		UserResponceModel returnValue = mapper.map(createdUser, UserResponceModel.class);
+		
+		return returnValue;
 	}
 	
 	@PutMapping("/{id}")
