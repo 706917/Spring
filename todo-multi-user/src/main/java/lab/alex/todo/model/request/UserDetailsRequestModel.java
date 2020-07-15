07de6c933cb26938f79cb9337cc98a68f9e58819
Alex <githubalexrom@gmail.com>
2020-07-15 09:36:58 -0400

    Create UserDetailsRequestModel.java

---
+++ todo-multi-user/src/main/java/lab/alex/todo/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,58 @@
+package lab.alex.todo.model.request;
+
+
+public class UserDetailsRequestModel {
+
+	// ******************Attributes********************
+
+	private String name;
+	private String phone;
+	private String email;
+
+	private String userMoto;
+
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	// **************** Accessors **********************
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserMoto() {
+		return userMoto;
+	}
+
+	public void setUserMoto(String userMoto) {
+		this.userMoto = userMoto;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
