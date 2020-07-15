2e2680fbd5c3383647f3d66dc59dbcb96a70e377
Alex <githubalexrom@gmail.com>
2020-07-15 09:38:38 -0400

    ModelMapper @Bean added as singleton

--- todo-multi-user/src/main/java/lab/alex/todo/TodoMultiUserApplication.java
+++ todo-multi-user/src/main/java/lab/alex/todo/TodoMultiUserApplication.java
@@ -1,13 +1,22 @@
 package lab.alex.todo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoMultiUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoMultiUserApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper mapper() {
+		ModelMapper mapper = null;
+		if (mapper == null) mapper = new ModelMapper();
+		return mapper;
 	}
 
 }
