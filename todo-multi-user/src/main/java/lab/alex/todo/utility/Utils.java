1bcdf4154134a35284ca022748eb583bde520f10
Alex <githubalexrom@gmail.com>
2020-07-15 09:38:45 -0400

    Create Utils.java

---
+++ todo-multi-user/src/main/java/lab/alex/todo/utility/Utils.java
@@ -0,0 +1,33 @@
+package lab.alex.todo.utility;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+	
+	private final String CHARACTERS = "0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+	private final Random RANDOM = new SecureRandom();
+	
+	
+	public String generatePublicUserId() {
+		int numberOfCharacters = 15;
+		return genarateRandomString(numberOfCharacters);
+	}
+	
+	
+	
+	
+	
+	private String genarateRandomString(int limit) {
+		StringBuilder returnValue = new StringBuilder(limit);		
+		
+		for (int i = 0; i<limit; i++) {
+			returnValue.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+		}
+		return returnValue.toString();
+	}
+
+}
