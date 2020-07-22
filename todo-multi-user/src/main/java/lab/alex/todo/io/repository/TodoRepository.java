c27b18954aff5e5e3cb384e537628f124c29e144
Alex <githubalexrom@gmail.com>
2020-07-22 11:30:46 -0400

    Create TodoRepository.java

---
+++ todo-multi-user/src/main/java/lab/alex/todo/io/repository/TodoRepository.java
@@ -0,0 +1,12 @@
+package lab.alex.todo.io.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import lab.alex.todo.io.entity.TodoEntity;
+
+
+@Repository
+public interface TodoRepository extends CrudRepository<TodoEntity, Long> {
+
+}
