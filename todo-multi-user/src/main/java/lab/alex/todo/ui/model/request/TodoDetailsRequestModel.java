96fa6f415fa27f84a2cfca2e6b658ea374ed5d0d
Alex <githubalexrom@gmail.com>
2020-08-11 08:22:50 -0400

    Create TodoDetailsRequestModel.java

---
+++ todo-multi-user/src/main/java/lab/alex/todo/ui/model/request/TodoDetailsRequestModel.java
@@ -0,0 +1,66 @@
+package lab.alex.todo.ui.model.request;
+
+import java.util.Date;
+
+
+public class TodoDetailsRequestModel {
+	
+	//********************Attributes***************
+	
+	private String textTodo;	
+	private Date dateTimeTodoCreated;
+	private boolean completed;
+	
+	private String public_user_id;
+	private String public_event_id;
+	private String public_group_id;
+	
+	
+	
+
+	//****************Accessors************************
+	
+	
+	
+	public String getTextTodo() {
+		return textTodo;
+	}
+	public void setTextTodo(String textTodo) {
+		this.textTodo = textTodo;
+	}
+	public Date getDateTimeTodoCreated() {
+		return dateTimeTodoCreated;
+	}
+	public void setDateTimeTodoCreated(Date dateTimeTodoCreated) {
+		this.dateTimeTodoCreated = dateTimeTodoCreated;
+	}
+	public boolean isCompleted() {
+		return completed;
+	}
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	public String getPublic_user_id() {
+		return public_user_id;
+	}
+	public void setPublic_user_id(String public_user_id) {
+		this.public_user_id = public_user_id;
+	}
+	public String getPublic_event_id() {
+		return public_event_id;
+	}
+	public void setPublic_event_id(String public_event_id) {
+		this.public_event_id = public_event_id;
+	}
+	public String getPublic_group_id() {
+		return public_group_id;
+	}
+	public void setPublic_group_id(String public_group_id) {
+		this.public_group_id = public_group_id;
+	}
+	
+	
+	
+	
+
+}
