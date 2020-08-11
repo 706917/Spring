cd5e94dc391c3599e76e6c09efa6dd5d0e95d105
Alex <githubalexrom@gmail.com>
2020-08-11 08:26:15 -0400

    Added annotation @Repository

--- todo-multi-user/src/main/java/lab/alex/todo/io/repository/UserRepository.java
+++ todo-multi-user/src/main/java/lab/alex/todo/io/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package lab.alex.todo.io.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import lab.alex.todo.io.entity.UserEntity;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	
 	UserEntity findByEmail(String email);
